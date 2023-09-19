1e4714906ec47e43727f95854eebfbbe16da52f6
｛masuda｝ <supika5555@gmail.com>
2023-09-19 23:28:26 +0900

    test 複数issueの自動クローズテスト

--- app/page.jsx
+++ app/page.jsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 const Home = () => {
   const router = useRouter();
   router.push(`/top`);
-  return <></>;
+  return <>複数issueの自動クローズテスト</>;
 };
 
 export default Home;
